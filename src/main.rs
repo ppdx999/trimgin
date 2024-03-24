41ad8e67415e132538047873bfa39df1222e82fa
ppdx999 <ppdx999@gmail.com>
2024-03-24 13:40:07 +0900

    support [File] option and help message

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,59 @@
-use std::io::{self, BufRead};
+use std::io::{self, BufReader, BufRead};
 
 fn is_blank(c: char) -> bool {
     c == ' ' || c == '\t' || c == '\n'
 }
 
 fn main() {
-    let stdin = io::stdin();
+    let first_arg = std::env::args().nth(1);
+
+    if let Some(arg) = &first_arg {
+        if arg == "-h" || arg == "--help" {
+            let help = r#"Usage: trimgin [FILE]
+
+Remove leading and trailing whitespace from text.
+If FILE is not provided, read from stdin.
+
+For example:
+
+```text.txt
+
+
+Hi there!
+We have two blank lines above and below this text.
+And this space will be removed by 'trimgin'.
+
+
+```
+
+```terminal
+$ trimgin text.txt
+Hi there!
+We have two blank lines above and below this text.
+And this space will be removed by 'trimgin'.
+```"#;
+            println!("{}", help);
+
+            std::process::exit(0);
+        }
+    }
+
+    let reader: Box<dyn BufRead> = match first_arg {
+        Some(file) => {
+            if file == "-" {
+                Box::new(BufReader::new(io::stdin()))
+            } else {
+                Box::new(BufReader::new(std::fs::File::open(file).unwrap()))
+            }
+        },
+        None => Box::new(BufReader::new(io::stdin())),
+    };
+
     let mut buf = String::new();
     let mut leading_blank = true;
 
-    for line in stdin.lock().lines() {
+    // Read line by line to avoid reading the whole file into memory
+    for line in reader.lines() {
         for c in line.unwrap().chars() {
             if leading_blank && is_blank(c) {
                 continue;
--- test/main.sh
+++ test/main.sh
@@ -4,6 +4,7 @@ set -e
 
 root_dir=$(dirname $(cd $(dirname $0) && pwd))
 test_dir=$root_dir/test
+cmd=$root_dir/target/debug/trimgin
 
 result=$(mktemp)
 trap 'rm -f $result' EXIT
@@ -15,27 +16,32 @@ test_fail() {
   exit 1
 }
 
-run_test() {
-  echo "Running test $1"
-
-  #
-  # Arrange & Act
-  # 
-  cat $1/in.txt | $root_dir/target/debug/trimgin > $result
-
-  #
-  # Assert
-  #
-  if [ "$(cat $1/out.txt)" != "$(cat $result)" ]; then
+assert_result() {
+  if [ "$(cat $1)" != "$(cat $result)" ]; then
     printf "\033[1;31mTest $1 failed\033[0m\n"
     echo
-    printf "\033[1;31mExpected stdout: \n$(cat $1/out.txt)\033[0m\n"
+    printf "\033[1;31mExpected stdout: \n$(cat $1)\033[0m\n"
     echo
     printf "\033[1;31mActual stdout  : \n$(cat $result) \033[0m\n"
     exit 1
   fi
 }
 
+run_test() {
+  echo "Running test $1"
+
+  #
+  # Arrange & Act
+  # 
+  cat $1/in.txt | $cmd > $result
+
+  #
+  # Assert
+  #
+  assert_result $1/out.txt
+}
+
+# Test filtering behavior
 run_test $test_dir/empty
 run_test $test_dir/un_trimmable
 run_test $test_dir/leading_blank
@@ -44,5 +50,16 @@ run_test $test_dir/leading_and_trailing_blank
 run_test $test_dir/leading_empty_lines
 run_test $test_dir/trailing_empty_lines
 run_test $test_dir/leading_and_trailing_empty_lines
+
+echo "Testing help message"
+$cmd -h > $result
+assert_result $test_dir/help.txt
+
+echo "Testing read from file"
+$cmd $test_dir/example.txt > $result
+assert_result $test_dir/example.txt
+
+echo "Testing reading from stdin with '-'"
+cat $test_dir/example.txt | $cmd - > $result
 
 printf "\033[1;32mAll tests passed\033[0m\n"
